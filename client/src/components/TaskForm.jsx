f69be7f3548ef78baa1a18e69faa7d2006ca27db
AmbilyThomas90 <ambilymariya.9@gmail.com>
2025-12-15 11:57:07 +0530

    Updated  project

--- client/src/components/TaskForm.jsx
+++ client/src/components/TaskForm.jsx
@@ -44,10 +44,13 @@ export default function TaskForm({ token, currentTask, setCurrentTask, refresh, 
         placeholder="✍️ Add a new task..."
         value={title}
         onChange={(e) => setTitle(e.target.value)}
-        className="flex-1 px-4 py-2 text-sm border border-gray-300 rounded-lg
+        className="flex-1 px-4 py-2 text-sm
+             border border-gray-300 rounded-lg
+             text-black placeholder-gray-500
              focus:outline-none focus:ring-2 focus:ring-blue-400
              resize-y transition"
       />
+
 
 
 
